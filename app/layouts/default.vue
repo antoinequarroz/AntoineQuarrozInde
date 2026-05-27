35305fe237b071d88f85001b4c1e2fa081f273b5
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2026-05-27 20:22:37 +0200

    chore: update site 2026-05-27 20:22

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -16,12 +16,6 @@ watchEffect(() => {
     <main>
       <slot />
     </main>
-    <a
-      href="/#contact"
-      class="md:hidden fixed bottom-[max(0.9rem,env(safe-area-inset-bottom))] left-3 right-3 z-40 btn-primary justify-center py-3 text-sm shadow-glow"
-    >
-      {{ t('hero.cta_primary') }}
-    </a>
     <LayoutAppFooter />
   </div>
 </template>
