da178452f5b93102c08d0c6a24aeca3bd6b3b44b
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2026-05-28 11:09:20 +0200

    chore: update site 2026-05-28 11:09

--- app/components/ui/ThemeToggle.vue
+++ app/components/ui/ThemeToggle.vue
@@ -13,7 +13,7 @@ function toggle() {
 <template>
   <button
     v-if="!isMobile"
-    class="relative w-10 h-10 rounded-xl flex items-center justify-center
+    class="relative w-8 h-8 rounded-lg flex items-center justify-center
            bg-gray-100 dark:bg-gray-800/50 hover:bg-violet-100 dark:hover:bg-violet-500/20
            text-gray-500 dark:text-gray-400 hover:text-violet-600 dark:hover:text-violet-400
            transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-violet-500/50"
@@ -21,7 +21,7 @@ function toggle() {
     @click="toggle"
   >
     <Transition name="theme-icon" mode="out-in">
-      <svg v-if="isDark" key="sun" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <svg v-if="isDark" key="sun" xmlns="http://www.w3.org/2000/svg" class="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <circle cx="12" cy="12" r="5" />
         <line x1="12" y1="1" x2="12" y2="3" />
         <line x1="12" y1="21" x2="12" y2="23" />
@@ -32,7 +32,7 @@ function toggle() {
         <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
         <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
       </svg>
-      <svg v-else key="moon" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <svg v-else key="moon" xmlns="http://www.w3.org/2000/svg" class="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
       </svg>
     </Transition>
